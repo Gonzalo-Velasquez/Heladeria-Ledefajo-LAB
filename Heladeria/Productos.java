9922e6929d5e0bce0563f20fd5a67d78e0df6a47
Jairo-Carbajal <168127511+Jairo-Carbajal@users.noreply.github.com>
2024-10-28 14:50:02 -0300

    Add files via upload

---
+++ Heladeria/Productos.java
@@ -0,0 +1,14 @@
+
+package heladeria;
+
+public class Productos {
+    private String Nombre;
+    private int Precio;
+
+    public Productos(String Nombre, int Precio) {
+        this.Nombre = Nombre;
+        this.Precio = Precio;
+    }
+ 
+    
+}
