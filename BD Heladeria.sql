14498464d400bf656282d17b279c28b9b878144a
Jairo-Carbajal <168127511+Jairo-Carbajal@users.noreply.github.com>
2024-11-13 15:25:29 -0300

    Update BD Heladeria.sql

--- BD Heladeria.sql
+++ BD Heladeria.sql
@@ -27,7 +27,7 @@ CREATE TABLE pedidos (
     tipo_pedido Enum('En Tienda','Delivery') NOT NULL,
     id_cliente INT, -- Solo para pedidos de tipo 'Delivery'
     direccion VARCHAR(255), -- Solo si es delivery
-    total DECIMAL(10, 2) NOT NULL,
+    total DECIMAL(10, 2),
     FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente) 
 );
 
